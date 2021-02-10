823c3f0b2cdc6a51351d11229432c07a11799721
rdarida <robertdarida86@gmail.com>
2021-02-10 22:09:25 +0100

    chore: uppercase component names

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Badge from "../components/badge"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+import Badge from "../components/Badge"
 
 const IndexPage = () => (
   <Layout>
