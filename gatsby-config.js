93a0f94bf2c18d584c099a211b99ce198696b9b1
rdarida <robertdarida86@gmail.com>
2021-02-10 18:25:51 +0100

    chore: update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `rdarida.github.io`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Róbert Darida's GitHub Profile`,
+    author: `Róbert Darida`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
