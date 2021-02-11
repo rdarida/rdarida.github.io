01a773cf598331bb8f49ac566a9b0d4a6f895d24
rdarida <robertdarida86@gmail.com>
2021-02-11 09:54:04 +0100

    chore: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `rdarida.github.io`,
-    description: `Róbert Darida's GitHub Profile`,
+    description: `Róbert Darida's GitHub Page`,
     author: `Róbert Darida`,
   },
   plugins: [
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-512x512.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
