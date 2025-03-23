2ff606c488f5c98d24247825a30d6c749dfae299
Róbert Darida <rdarida@users.noreply.github.com>
2025-03-23 10:54:44 +0100

    feat: add inner container to Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 
 export const Footer: FC = (): JSX.Element => (
-  <footer className="container mt-3 border-top">
-    <div className="row">
-      <div className="col-12 py-3 text-end">
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://nextjs.org">NextJS</a>
+  <footer className="mt-3 border-top">
+    <div className="container">
+      <div className="row">
+        <div className="col-12 py-3 text-end">
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://nextjs.org">NextJS</a>
+        </div>
       </div>
     </div>
   </footer>
