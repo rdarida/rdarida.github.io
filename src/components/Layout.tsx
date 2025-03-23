6bf3a8aae64b0a49b7f6ad0de454b13fd08c2dcb
Róbert Darida <rdarida@users.noreply.github.com>
2025-03-23 11:14:35 +0100

    feat: simplify Layout component's structure

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,17 +10,7 @@ type LayoutProps = {
 export const Layout: FC<LayoutProps> = ({ children }): JSX.Element => (
   <>
     <Header siteTitle="rdarida.github.io" />
-
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0 1.0875rem 1.45rem`
-      }}
-    >
-      <main>{children}</main>
-    </div>
-
+    <main>{children}</main>
     <Footer />
   </>
 );
